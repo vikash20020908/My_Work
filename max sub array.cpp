973040c80f6e33a75ae96d82d9be0917f4388a0e
vikash20020908 <vikash20020908@gmail.com>
2022-03-17 23:22:48 +0530

    max sub array

--- max sub array.cpp
+++ max sub array.cpp
@@ -1,18 +1,20 @@
-#include<stdio.h>
-#include<conio.h>
+#include <iostream>
+using namespace std ;
 
 int main()
 {
+	cout<<"enter the size of array"<<endl;
 	int n; 
 	cin>>n;
 	int A[n];
+	cout<<"enter the element"<<endl;
+	
 	
 	for (int i=0;i<n;i++)
-	{
-		cin>>A[i]
-	}
+		cin>>A[i];
+
 	int ans=2;
-	int pd=A[i]-A[0];
+	int pd= A[1]- A[0];
 	int j=2;
 	int curr=2;
 	
